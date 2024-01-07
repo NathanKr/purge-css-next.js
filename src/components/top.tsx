a42a3516d4880c457f067809ee9c4f3cdd9923f9
nathankr <natankrasney@gmail.com>
2024-01-07 20:59:54 +0200

    better top

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { useRouter } from "next/router";
 import PageTabs from "@/types/e-page-tabs";
 import PageNestedTabs from "@/types/e-page-nested-tabs";
+import Link from "next/link";
+import styles from "@/styles/top.module.css";
 
 interface ITabMenu {
   href: string;
@@ -10,11 +11,13 @@ interface ITabMenu {
 
 const Top = () => {
   const [navExpanded, setNavExpanded] = useState(false);
-  const router = useRouter();
 
   const tabs: ITabMenu[] = [
     { href: `/${PageTabs.home}`, text: "Home" },
-    { href: `/${PageTabs.courses}/${PageNestedTabs.arbitrator}`, text: "Courses" },
+    {
+      href: `/${PageTabs.courses}/${PageNestedTabs.arbitrator}`,
+      text: "Courses",
+    },
     { href: `/${PageTabs.events}`, text: "Events" },
     { href: `/${PageTabs.testimonials}`, text: "Testimonials" },
     { href: `/${PageTabs.blog}`, text: "Blog" },
@@ -27,43 +30,52 @@ const Top = () => {
     setNavExpanded(!navExpanded);
   };
 
-  const handleNavItemClick = (href: string) => {
-    setNavExpanded(false);
-    router.push(href);
-  };
-
   const elemTabs = tabs.map((it, i) => (
-    <a key={i} onClick={() => handleNavItemClick(it.href)} className="nav-link">
+    <Link
+      key={i}
+      href={it.href}
+      onClick={() => setNavExpanded(false)}
+      className={styles.navigationLink}
+    >
       {it.text}
-    </a>
+    </Link>
   ));
 
   return (
-    <nav className="navbar navbar-expand-xl navbar-dark bg-dark">
-      <div className="container">
-        <h1 className="navbar-brand">Nathan Krasney</h1>
-        <button
-          className="navbar-toggler"
-          type="button"
-          aria-controls="responsive-navbar-nav"
-          aria-expanded={navExpanded ? "true" : "false"}
-          aria-label="Toggle navigation"
-          onClick={handleNavToggle}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${navExpanded ? "show" : ""}`}>
-          <ul className="navbar-nav ml-auto">{elemTabs}</ul>
-        </div>
-      </div>
-    </nav>
+    <div className={styles.Top}>
+      <header className={styles.header}>
+        <nav className="navbar navbar-expand-xl navbar-dark bg-dark">
+          <div className="container">
+            <div className="navbar-brand">
+              <h1>
+                <span className={styles.nameColor}>Nathan</span> Krasney
+              </h1>
+            </div>
+            <button
+              className="navbar-toggler"
+              type="button"
+              aria-controls="responsive-navbar-nav"
+              aria-expanded={navExpanded ? "true" : "false"}
+              aria-label="Toggle navigation"
+              onClick={handleNavToggle}
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className={`collapse navbar-collapse ${
+                navExpanded ? "show" : ""
+              }`}
+            >
+              <ul className="navbar-nav ml-auto">{elemTabs}</ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
   );
 };
 
 export default Top;
-
-
-
 
 // import { useState } from "react";
 // import styles from "@/styles/top.module.css";
