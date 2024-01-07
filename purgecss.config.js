585aa2c261103d8551ca234a9d40df9f117ff0a1
nathankr <natankrasney@gmail.com>
2024-01-07 21:07:48 +0200

    add bootstrap.css as content because not working on vercel

--- purgecss.config.js
+++ purgecss.config.js
@@ -6,11 +6,10 @@ module.exports = {
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
-  // css: ["./node_modules/bootstrap/dist/css/bootstrap.css"],
+  css: ["./node_modules/bootstrap/dist/css/bootstrap.css"],
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   safelist: {
     standard: ["html", "body"],
-    // deep: [...alertComponentSelectors, ...navbarComponentSelectors],
   },
   output: "./node_modules/bootstrap/dist/css/bootstrap-with-purgecss.css",
 };
