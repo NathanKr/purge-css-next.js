49791effc6b2a384e927544dad5e52bac81a9858
nathankr <natankrasney@gmail.com>
2024-01-08 10:00:02 +0200

    remove defaultExtractor and safelist

--- purgecss.config.js
+++ purgecss.config.js
@@ -5,9 +5,5 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   css: ["./node_modules/bootstrap/dist/css/bootstrap.css"],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  safelist: {
-    standard: ["html", "body"],
-  },
   output: "./node_modules/bootstrap/dist/css/bootstrap-with-purgecss.css",
 };
